8a5a87197cb1d931e1e240fb624db7108fe587e3
MohammadNikmard <mnikmard2004@gmail.com>
2024-09-04 17:02:24 +0330

    feature: add slider and switch and main logics

--- app/src/main/java/com/example/bmicalculator/homeScreen.kt
+++ app/src/main/java/com/example/bmicalculator/homeScreen.kt
@@ -1,7 +1,14 @@
 package com.example.bmicalculator
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -26,15 +33,31 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -45,6 +68,22 @@ import com.example.bmicalculator.ui.theme.interRegular
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreenContent(navController: NavController) {
+    var ageNumber by remember {
+        mutableStateOf(18)
+    }
+
+    var weightNumber by remember {
+        mutableStateOf(68)
+    }
+
+    var heightAmount by remember {
+        mutableStateOf(50f)
+    }
+
+    var genereSection by remember {
+        mutableStateOf(false)
+    }
+
     Scaffold(
         containerColor = Color(0xfff4f5ff),
         topBar = {
@@ -70,15 +109,26 @@ fun HomeScreenContent(navController: NavController) {
         ) {
             Spacer(modifier = Modifier.height(30.dp))
 
-            AgeWeightSection()
+            AgeWeightSection(
+                onAgeChanged = { value -> ageNumber = value },
+                onWeightChanged = { value -> weightNumber = value },
+            )
 
             Spacer(modifier = Modifier.height(23.dp))
 
-            HeightSection()
+            HeightSection(
+                onHeightChanged = { value ->
+                    heightAmount = value
+                },
+            )
 
             Spacer(modifier = Modifier.height(23.dp))
 
-            GenderSection()
+            GenderSection(
+                onGenderChnage = { value ->
+                    genereSection = value
+                },
+            )
 
             Spacer(modifier = Modifier.height(30.dp))
 
@@ -105,7 +155,17 @@ fun HomeScreenContent(navController: NavController) {
 }
 
 @Composable
-private fun AgeWeightSection() {
+private fun AgeWeightSection(
+    onAgeChanged: (Int) -> Unit,
+    onWeightChanged: (Int) -> Unit,
+) {
+    var ageNumber by remember {
+        mutableStateOf(18)
+    }
+
+    var weightAmount by remember {
+        mutableStateOf(68)
+    }
     Row {
         Card(
             colors = CardDefaults.cardColors(
@@ -138,7 +198,7 @@ private fun AgeWeightSection() {
                 Spacer(modifier = Modifier.height(3.dp))
 
                 Text(
-                    text = "30",
+                    text = ageNumber.toString(),
                     style = TextStyle(
                         fontSize = 57.sp,
                         color = Color(0xff6C63FF),
@@ -154,7 +214,12 @@ private fun AgeWeightSection() {
                         colors = IconButtonDefaults.iconButtonColors(
                             containerColor = Color(0xff081854)
                         ),
-                        onClick = {}) {
+                        onClick = {
+                            if (ageNumber >= 1) {
+                                ageNumber--
+                                onAgeChanged(ageNumber)
+                            }
+                        }) {
                         Image(
                             modifier = Modifier.size(26.dp),
                             painter = painterResource(id = R.drawable.icon_minus),
@@ -171,7 +236,12 @@ private fun AgeWeightSection() {
                         colors = IconButtonDefaults.iconButtonColors(
                             containerColor = Color(0xff081854)
                         ),
-                        onClick = {}) {
+                        onClick = {
+                            if (ageNumber <= 110) {
+                                ageNumber++
+                                onAgeChanged(ageNumber)
+                            }
+                        }) {
                         Image(
                             modifier = Modifier.size(26.dp),
                             painter = painterResource(id = R.drawable.icon_add),
@@ -215,7 +285,7 @@ private fun AgeWeightSection() {
                 Spacer(modifier = Modifier.height(3.dp))
 
                 Text(
-                    text = "78",
+                    text = weightAmount.toString(),
                     style = TextStyle(
                         fontSize = 57.sp,
                         color = Color(0xff6C63FF),
@@ -231,7 +301,12 @@ private fun AgeWeightSection() {
                         colors = IconButtonDefaults.iconButtonColors(
                             containerColor = Color(0xff081854)
                         ),
-                        onClick = {}) {
+                        onClick = {
+                            if (weightAmount >= 30) {
+                                weightAmount--
+                                onWeightChanged(weightAmount)
+                            }
+                        }) {
                         Image(
                             modifier = Modifier.size(26.dp),
                             painter = painterResource(id = R.drawable.icon_minus),
@@ -248,7 +323,10 @@ private fun AgeWeightSection() {
                         colors = IconButtonDefaults.iconButtonColors(
                             containerColor = Color(0xff081854)
                         ),
-                        onClick = {}) {
+                        onClick = {
+                            weightAmount++
+                            onWeightChanged(weightAmount)
+                        }) {
                         Image(
                             modifier = Modifier.size(26.dp),
                             painter = painterResource(id = R.drawable.icon_add),
@@ -261,12 +339,16 @@ private fun AgeWeightSection() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun HeightSection() {
+private fun HeightSection(onHeightChanged: (Float) -> Unit) {
+    var heightMetrick by remember {
+        mutableStateOf(50f)
+    }
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .height(183.dp),
+            .height(190.dp),
         shape = RoundedCornerShape(12.dp),
         colors = CardDefaults.cardColors(
             containerColor = Color.White,
@@ -292,13 +374,55 @@ private fun HeightSection() {
                     fontSize = 57.sp, fontFamily = interBold, color = Color(0xff6C63FF),
                 ),
             )
+
+            Slider(
+                colors = SliderDefaults.colors(
+                    thumbColor = Color(0xff6C63FF),
+                    activeTrackColor = Color(0xffCCCCCC),
+                    inactiveTrackColor = Color(0xffCCCCCC),
+                ),
+                valueRange = 50f..300f,
+                value = heightMetrick, onValueChange = { value ->
+                    heightMetrick = value
+                    onHeightChanged(heightMetrick)
+                },
+                thumb = {
+                    val interactionSource = remember {
+                        MutableInteractionSource()
+                    }
+                    SliderDefaults.Thumb(
+                        interactionSource = interactionSource,
+                        thumbSize = DpSize(27.dp, 27.dp),
+                    )
+                }
+            )
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                Text(
+                    "50 cm",
+                    style = TextStyle(
+                        fontFamily = interRegular, fontSize = 13.sp, color = Color(0xff081854),
+                    ),
+                )
+                Text(
+                    "300cm",
+                    style = TextStyle(
+                        fontFamily = interRegular, fontSize = 13.sp, color = Color(0xff081854),
+                    ),
+                )
+
+            }
         }
     }
 }
 
 
 @Composable
-private fun GenderSection() {
+private fun GenderSection(onGenderChnage: (Boolean) -> Unit) {
+
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -311,7 +435,7 @@ private fun GenderSection() {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(horizontal = 45.dp),
+                .padding(horizontal = 30.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Spacer(modifier = Modifier.height(25.dp))
@@ -323,11 +447,13 @@ private fun GenderSection() {
                 ),
             )
 
-            Spacer(modifier = Modifier.height(26.dp))
+            Spacer(modifier = Modifier.height(16.dp))
 
             Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxWidth(),
             ) {
                 Text(
                     "Male",
@@ -337,6 +463,13 @@ private fun GenderSection() {
                         color = Color(0xff081854),
                     ),
                 )
+                Spacer(modifier = Modifier.width(40.dp))
+
+                Switch2(
+                    onGenderChnage = { onGenderChnage(it) }
+                )
+
+                Spacer(modifier = Modifier.width(32.dp))
                 Text(
                     "Female",
                     style = TextStyle(
@@ -348,5 +481,63 @@ private fun GenderSection() {
             }
 
         }
+    }
+}
+
+
+@Composable
+fun Switch2(
+    scale: Float = 2f,
+    width: Dp = 55.dp,
+    height: Dp = 20.dp,
+    checkedTrackColor: Color = Color(0xffF4F3FF),
+    uncheckedTrackColor: Color = Color(0xffF4F3FF),
+    gapBetweenThumbAndTrackEdge: Dp = 4.dp,
+    onGenderChnage: (Boolean) -> Unit,
+) {
+
+    var isMale by remember {
+        mutableStateOf(true) // Initially the switch is ON
+    }
+
+    val thumbRadius = (height / 2) - gapBetweenThumbAndTrackEdge
+
+    // To move thumb, we need to calculate the position (along x axis)
+    val animatePosition = animateFloatAsState(
+        targetValue = if (isMale)
+            with(LocalDensity.current) { (width - thumbRadius - gapBetweenThumbAndTrackEdge).toPx() }
+        else
+            with(LocalDensity.current) { (thumbRadius + gapBetweenThumbAndTrackEdge).toPx() }
+    )
+
+    Canvas(
+        modifier = Modifier
+            .size(width = width, height = height)
+            .scale(scale = scale)
+            .pointerInput(Unit) {
+                detectTapGestures(
+                    onTap = {
+                        isMale = !isMale
+                        onGenderChnage(isMale)
+                    }
+                )
+            }
+    ) {
+        // Track
+        drawRoundRect(
+            color = if (isMale) checkedTrackColor else uncheckedTrackColor,
+            cornerRadius = CornerRadius(x = 10.dp.toPx(), y = 10.dp.toPx()),
+
+            )
+
+        // Thumb
+        drawCircle(
+            color = Color(0xff6C63FF),
+            radius = thumbRadius.toPx(),
+            center = Offset(
+                x = animatePosition.value,
+                y = size.height / 2
+            )
+        )
     }
 }
